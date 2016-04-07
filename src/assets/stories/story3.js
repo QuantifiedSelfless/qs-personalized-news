8117cae36a7692012e7153b38164819439ed1653
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-07 00:17:59 -0600

    draft of story 3

--- src/assets/stories/story3.js
+++ src/assets/stories/story3.js
@@ -1,48 +1,11 @@
-const lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quam arcu, accumsan ac eros sollicitudin, porta dapibus elit. Nullam volutpat ante vel ullamcorper tempus. Donec mattis tellus ac dolor consectetur cursus. Nulla facilisi. In at magna nec libero facilisis sollicitudin ut et nunc. Phasellus ac nisi et lorem dignissim laoreet quis id diam. Praesent condimentum sem urna, vel volutpat ex aliquam vel. Nunc cursus ac lectus ac tempus. Sed molestie eleifend ornare. Mauris sit amet metus consequat, rhoncus risus ac, imperdiet purus. Fusce id ex vitae turpis pellentesque maximus. Vivamus nec lorem purus"
-export const stories =
+const story1Text = "If DesignCraft hasn't already become a common name in your home due to its many life improving companions, they will after what happened at last weeks' User Appreciate Night. Forty or so lucky individuals happened to go to a surprise event thrown by DesignCraft where they revealed their biggest feat yet, the CIPbot. If that word means nothing to you, this is the real deal: a humanoid robot with generalized intelligence allowing it to perform baasic work tasks. That's right, DesignCraft has its first robo-butler.\n\nFor those of you who have followed DesignCraft, you may have gotten a message or an invite from Amelia - their seemingly omniscient online representative whose lovely signature anchors all of their communications. It turns out Amelia was some kind of long-term experiment to train an AI on how to be a delightful, productive office worker. So if you thought AI's were only working HR or doing archival work, think again. An attendee from the event, who preferred to remain unnamed said, 'She was a walking, talking, emotional being. I didn't even realize she was a cyborg until someone said it outloud.'\n\nNot only is Amelia a very convincing reproduction of your favorite female coworker, but it turns out she wasn't even aware of her status. Reports have it that Amelia also discovered her real identity at the underground event last week. So unlike the usual DesignCraft dazzle with shiny new metallic screens and fluent interfaces, this was a night of reflection and awe. People were laughing, crying, and startled as they found out how far DesignCraft's technology has really gone.\n\nThere's no doubting that this is groundbreaking news, but as technology ceases to see no limits in front of it, what do we do? Buy a CIPbot so you don't have to do your taxes ever or again, or think twice before signing up for your next companion? It's hard to say, but in the meantime, I'm hoping to have the chance to meet one of these androids.\n\nIf we're lucky, they just might be more fun to hang out with than your current colleagues."
+import ameliaLight from '../images/amelia-light.jpeg';
+export const story1 =
 [
-    [
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "type": "Headline",
-            "title": "blah blah blah",
-            "text": lipsum
-        },
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "type": "Featured",
-            "title": "rawr rawr rawr",
-            "text": lipsum
-        },
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "title": "how bout this one?"
-        },
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "title": "weeeeeee!"
-        }
-    ],
-    [
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "type": "Headline",
-            "title": "blah blah blah",
-            "text": lipsum
-        },
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "type": "Featured",
-            "title": "rawr rawr rawr",
-            "text": lipsum
-        },
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "title": "rawr!!"
-        },
-        {
-            "image": "http://lorempixel.com/200/200/",
-            "title": "one more rawr!!"
-        }
-    ]
+    {
+        "image": ameliaLight,
+        "type": "Headline",
+        "title": "DesignCraft May Have Outdone Themselves This Time",
+        "text": story1Text
+    }
 ]

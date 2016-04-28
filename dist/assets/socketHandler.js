fc8496c30a334b2b5723c25cb6e8b6824f28b5a8
michaelaaron <michaelaaron@michaelaaron-macbookpro.roam.corp.google.com>
2016-04-27 21:11:16 -0600

    removed date from title and page

--- dist/assets/socketHandler.js
+++ dist/assets/socketHandler.js
@@ -1,6 +1,15 @@
 var socket = io.connect('http://localhost:3000');
+var timeoutHandler = null;
+
 socket.on('rfid', function(data){
     setTimeout(function() {window.location = "http://localhost:8000"}, 2000)
 });
 //auto logout
-setTimeout(function() {window.location="http://localhost:8000"}, 120000);
+timeoutHandler = setTimeout(function() {window.location="http://localhost:8000"}, 60000);
+//reset the timer onscroll
+window.onscroll = function() {
+    console.log("scrolled");
+    window.clearTimeout(timeoutHandler);
+    timeoutHandler = window.setTimeout(function() {window.location="http://localhost:8000"}, 60000);
+}
+

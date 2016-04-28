c7b2507fdbe19e2c4ef9dffec7ef13dffa196c17
michaelaaron <michaelaaron@michaelaaron-macbookpro.roam.corp.google.com>
2016-04-27 20:06:51 -0600

    hopefully added auto-loggout after 2 minutes

--- dist/assets/socketHandler.js
+++ dist/assets/socketHandler.js
@@ -2,5 +2,5 @@ var socket = io.connect('http://localhost:3000');
 socket.on('rfid', function(data){
     setTimeout(function() {window.location = "http://localhost:8000"}, 2000)
 });
-
-
+//auto logout
+setTimeout(function() {window.location="http://localhost:8000"}, 120000);
